d6ca1d25ed9df6ae11fa25043305ac82f5bf6e36
LordVillain <danyailyinchyk2005@gmail.com>
2025-03-07 00:21:04 +0300

    добавил refresh token без обновления, валидации и прокидывания

--- pkg/di/interfaces.go
+++ pkg/di/interfaces.go
@@ -5,11 +5,6 @@ import (
 	"github.com/ShopOnGO/ShopOnGO/prod/internal/product"
 	"github.com/ShopOnGO/ShopOnGO/prod/internal/user"
 )
-import (
-	"github.com/ShopOnGO/ShopOnGO/prod/internal/category"
-	"github.com/ShopOnGO/ShopOnGO/prod/internal/product"
-	"github.com/ShopOnGO/ShopOnGO/prod/internal/user"
-)
 
 type IStatRepository interface {
 	AddClick(linkId uint)
@@ -18,19 +13,6 @@ type IStatRepository interface {
 type IUserRepository interface {
 	Create(user *user.User) (*user.User, error)
 	FindByEmail(email string) (*user.User, error)
-}
-
-type IProductRepository interface {
-	Create(product *product.Product) (*product.Product, error)
-	GetByCategory(category *category.Category) ([]product.Product, error)
-	GetByName(name string) ([]product.Product, error)
-	GetFeaturedProducts(amount uint, random bool) ([]product.Product, error)
-}
-
-type ICategoryRepository interface {
-	Create(category *category.Category) (*category.Category, error)
-	GetFeaturedCategories(amount int) ([]category.Category, error)
-	FindByName(name string) (*category.Category, error)
 }
 
 type IProductRepository interface {
